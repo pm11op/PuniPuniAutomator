4837b8e5181fdb437009a66d78824f5c090705f2
Kinoshit <masaru@MasaruBookPro.local>
2016-07-14 15:24:23 +0900

    execute on the debice

--- battle.py
+++ battle.py
@@ -8,6 +8,9 @@ class Puni:
   _Y = (1030, 1350)
   swipeLength = 125
   swipeWeight = 4
+  screenShot = './img/ss.png'
+  _R = 0
+  _cmd = []
 
   def execute(self, cmd):
     if self.debug == 0:
@@ -31,11 +34,10 @@ class Puni:
   #
   ##
   def swipe(self, pos=None):
-    r = random.randint(0, 10)
-    if r < 7 :
+    if self._R < 7 :
       return
     vector = 1
-    if r%2 == 0:
+    if self._R%2 == 0:
       vector = -1
 
     if not pos:
@@ -52,19 +54,43 @@ class Puni:
       x2 = random.randint(min(self._X[1], max(self._X[0], pos['x'] + vector*self.swipeLength*self.swipeWeight)), min(self._X[1], max(self._X[0], pos['x'] + vector*self.swipeLength)))
       y2 = random.randint(min(self._Y[1], max(self._Y[0], pos['y'] + vector*self.swipeLength*self.swipeWeight)), min(self._Y[1], max(self._Y[0], pos['y'] + vector*self.swipeLength)))
 
+    if pos['x'] == x2 and pos['y'] == y2:
+      return
+    
     cmd = cmdf % {'x1': pos['x'], 'y1': pos['y'], 'x2': x2, 'y2': y2}
     self.execute(cmd)
 
-    if r >= 9:
+    if self._R >= 9:
       self.swipe({'x': x2, 'y': y2})
         
 
   def isFinished(self):
     return False
-  
+
+  def onLoopInit(self):
+    self._R = random.randint(0, 10)
+
+  def takeScreenShot(self):
+#    if self._R >= 3:
+#      return
+        
+    cmd1 = 'adb shell screencap -p /sdcard/screen.png'
+    cmd2 = 'adb pull /sdcard/screen.png %(imgpath)s' % {'imgpath': self.screenShot}
+    self.execute(cmd1)
+    self.execute(cmd2)
+
     
+import subprocess
+
 Puni = Puni()
+p = subprocess.Popen(['adb', 'shell', 'sh /sdcard/touch.sh'])
 while (1):
+#  Puni.takeScreenShot()
+  if p.poll() is 0 :
+    p = subprocess.Popen(['adb', 'shell', 'sh /sdcard/touch.sh'])
+  continue
+  Puni.onLoopInit()
+  Puni.takeScreenShot()
   if Puni.isFinished():
     exit()
   Puni.swipe()
