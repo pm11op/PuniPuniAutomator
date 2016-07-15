3bf57df4e15047ea99b7e2483c053a9a42ff73d3
Kinoshit <masaru@MasaruBookPro.local>
2016-07-15 16:24:28 +0900

    using dd made click fastest

--- battle.py
+++ battle.py
@@ -19,9 +19,12 @@ class Puni:
   _macro = []
   _swipeTrack = []
   _px_fin = [(18, 40), (533, 40), (1053, 40)]
-  _col_fin = [250, 213, 113] #BGR
+  _col_fin = (250, 213, 113) #BGR
+  _cols_special = [(62, 251, 226), (185, 254, 230)] #BGR  
   _col_margin = 15
   _flag_fin = False
+  _my_yokais = [(175, 614), (373, 558), (590, 540), (801, 560), (1019, 616)]
+  img = []
     
   def execute(self, cmd):
     if self.debug == 0:
@@ -37,141 +40,14 @@ class Puni:
     cmd = cmdf % {'x': x, 'y': y}
     self.execute(cmd)
     
-  def touchEvent(self):
-    cmdf = """
-sendevent /dev/input/event5 1 330 1
-sendevent /dev/input/event5 3 58 1
-sendevent /dev/input/event5 3 53 %(x)d
-sendevent /dev/input/event5 3 54 %(y)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-sendevent /dev/input/event5 1 330 0
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-"""[1:-1]
-
-#    x = random.randint(self._X[0], self._Y[0])
-#    y = random.randint(self._X[1], self._Y[1])
-    x = 412
-    y = 1600
-    cmd = cmdf % {'x': x, 'y': y}
-    self.intoMacro(cmd)
-
-
-  ##
-  #
-  ##
-  def swipeEvent(self, pos=None):
-    if self._R < 7 :
-      return
-    self.swipeSimple()
-#    self.swipeRandom(pos)
-
-  def swipeSimple(self):
-    cmdf = """
-sendevent /dev/input/event5 1 330 1
-sendevent /dev/input/event5 3 58 1
-sendevent /dev/input/event5 1 330 1
-sendevent /dev/input/event5 3 58 1
-sendevent /dev/input/event5 3 53 %(x1)d
-sendevent /dev/input/event5 3 54 %(y1)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-sendevent /dev/input/event5 3 53 %(x2)d
-sendevent /dev/input/event5 3 54 %(y2)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-sendevent /dev/input/event5 1 330 0
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-"""[1:-1]
-    pattern = [((508, 1544), (497, 1656)),
-               ((470, 1548), (535, 1616)),
-               ((537, 1544), (497, 1609)),
-               ((462, 1632), (353, 1600)),
-               ((527, 1655), (708, 1629))]
-    p  = random.randint(0, len(pattern)-1)
-    cmd = cmdf % {'x1': pattern[p][0][0], 'y1': pattern[p][0][1], 'x2': pattern[p][1][0], 'y2': pattern[p][1][1]}
-    self.intoMacro(cmd)
-
-    
-    
-  def swipeRandom(self, pos=None):
-    if self._SR == 7:
-      (vectorX, vectorY) = (-1, -1)
-    elif self._SR == 8:
-      (vectorX, vectorY) = (-1, 1)
-    elif self._SR == 9:
-      (vectorX, vectorY) = (1, 1)
-    elif self._SR == 10:
-      (vectorX, vectorY) = (1, -1)
-      
-    if not pos:
-      self._swipeTrack = []
-      pos = {}
-      pos['x'] = random.randint(self._X[0], self._X[1])
-      pos['y'] = random.randint(self._Y[0], self._Y[1])
-
-      cmdf = """
-sendevent /dev/input/event5 1 330 1
-sendevent /dev/input/event5 3 58 1
-sendevent /dev/input/event5 1 330 1
-sendevent /dev/input/event5 3 58 1
-sendevent /dev/input/event5 3 53 %(x1)d
-sendevent /dev/input/event5 3 54 %(y1)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-sendevent /dev/input/event5 3 53 %(x2)d
-sendevent /dev/input/event5 3 54 %(y2)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-"""[1:-1]
-      
-    else:
-      cmdf = """
-sendevent /dev/input/event5 3 53 %(x2)d
-sendevent /dev/input/event5 3 54 %(y2)d
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-"""[1:-1]
-
-    x2 = random.randint(min(self._X[1], max(self._X[0], min(pos['x'] + vectorX*self.swipeLength, pos['x'] + vectorX*self.swipeLength*self.swipeWeight))), min(self._X[1], max(self._X[0], max(pos['x'] + vectorX*self.swipeLength, pos['x'] + vectorX*self.swipeLength*self.swipeWeight))))
-    y2 = random.randint(min(self._Y[1], max(self._Y[0], min(pos['y'] + vectorY*self.swipeLength, pos['y'] + vectorY*self.swipeLength*self.swipeWeight))), min(self._Y[1], max(self._Y[0], max(pos['y'] + vectorY*self.swipeLength, pos['y'] + vectorY*self.swipeLength*self.swipeWeight))))
-
-#    print ('x', pos['x'], vectorX*self.swipeLength, x2)
-#    print ('y', pos['y'], vectorY*self.swipeLength, y2)    
-    
-    if pos['x'] == x2 and pos['y'] == y2 or pos['x'] in self._X or pos['y'] in self._Y :
-      self.swipeEnd()
-      return
-    
-    cmd = cmdf % {'x1': pos['x'], 'y1': pos['y'], 'x2': x2, 'y2': y2}
-    self._swipeTrack.append(cmd)
-
-    if self._SR >= 9:
-      self.genSRNum()
-      self.swipeRandom({'x': x2, 'y': y2})
-    else:
-      self.swipeEnd()
-
-  def swipeEnd(self):
-    cmd = """
-sendevent /dev/input/event5 1 330 0
-sendevent /dev/input/event5 0 2 0
-sendevent /dev/input/event5 0 0 0
-"""[1:-1]
-    self._swipeTrack.append(cmd)
-    cmd = "\n".join(self._swipeTrack)
-    self.intoMacro(cmd)
 
   def isFinished(self):
-    if not self._flag_fin:
+    if not self._flag_fin or len(self.img) is 0:
       return False
     
-    img = cv2.imread(self.screenShot)
     for yx in self._px_fin:
-      pixelBGR = img[yx[1], yx[0]]
-      if not self.compareColor(pixelBGR):
+      pixelBGR = self.img[yx[1], yx[0]]
+      if not self.compareColor(self._col_fin, pixelBGR):
         return False
     return True      
 #      print pixelBGR
@@ -186,14 +62,14 @@ sendevent /dev/input/event5 0 0 0
     self._P.kill()
     exit()
     
-  def compareColor(self, col):
-    b = self._col_fin[0] - col[0]
+  def compareColor(self, col1, col2):
+    b = col1[0] - col2[0]
     if abs(b) > self._col_margin:
       return False
-    g = self._col_fin[1] - col[1]
+    g = col1[1] - col2[1]
     if abs(g) > self._col_margin:
       return False
-    r = self._col_fin[2] - col[2]
+    r = col1[2] - col2[2]
     if abs(r) > self._col_margin:
       return False
     return True
@@ -201,15 +77,12 @@ sendevent /dev/input/event5 0 0 0
 
     
   def genRNum(self):
-    self.genSRNum()
     self._R = random.randint(0, 10)
     
-  def genSRNum(self):
-    self._SR = random.randint(7, 10)
 
   def takeScreenShot(self):
-    if self._R >= 2:
-      return
+#    if self._R >= 2:
+#      return
     
     self._flag_fin = True
         
@@ -218,43 +91,48 @@ sendevent /dev/input/event5 0 0 0
     self.execute(cmd1)
     self.execute(cmd2)
 
-  def intoMacro(self, cmd):
-    self._macro.append(cmd)
-    
-  def makeMacro(self):
-    self._macro = []
-    i = 0
-    while i < self.macroNum:
-      self.genRNum()
-      self.swipeEvent()
-      self.touchEvent()
-      i += 1
-    txt = "\n".join(self._macro)
-    f = open(self.macroPath + self.macroName, 'w')
-    f.write(txt)
-    f.close()
-    
-  def pushMacro(self):
-    cmd = 'adb push %(path)s%(macro)s /sdcard/' % {'path': self.macroPath, 'macro': self.macroName}
-    self.execute(cmd)
 
   def battleStart(self):
     self.touch(650, 1300)
     self._flag_fin = False
-    time.sleep(5)
+    time.sleep(8)
 
+  def doSpecial(self, num):
+    self.touch(self._my_yokais[num][0], self._my_yokais[num][1])
+    print 'do special #%d' % num
+
+  def checkSpecialGage(self):
+    if len(self.img) is 0:
+      return
+    
+    for i, yokai in enumerate(self._my_yokais):
+      pixelBGR = self.img[yokai[1], yokai[0]]
+      for color in self._cols_special:
+        if self.compareColor(color, pixelBGR):
+          self.killMacro()        
+          self.doSpecial(i)
+          self.doMacro()
+          return
+        
+    
   def doMacro(self):
-    self.battleStart()
     self._P = subprocess.Popen(['adb', 'shell', 'sh /sdcard/%(macro)s' % {'macro': self.macroName}])
-
+    
+  def killMacro(self):
+    self._P.kill()
+      
   def onLoop(self):
+    if self._flag_fin:
+      self.img = cv2.imread(self.screenShot)
+  
     if self._P.poll() is 0:
       print 'loop!!'
-      self._P = subprocess.Popen(['adb', 'shell', 'sh /sdcard/%(macro)s' % {'macro': self.macroName}])
+      self.doMacro()
 
+    return
+  
 Puni = Puni()
-Puni.makeMacro()
-Puni.pushMacro()
+Puni.battleStart()
 Puni.doMacro()
 
 
@@ -263,8 +141,8 @@ while (1):
   Puni.onLoop()
   Puni.genRNum()
   Puni.takeScreenShot()
+  Puni.checkSpecialGage()
   if Puni.isFinished():
     Puni.finish()
   
-  time.sleep(1)
-  
+  time.sleep(2)
